1e6a4887714434acbd1b47bfca1a038c8cba4363
Allan Gonçalves <allangoncalves.adm@gmail.com>
2024-02-11 18:12:29 -0300

    Add table, pagination, order page, update sooner

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import './global.css'
 
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
-import { Toaster } from 'sonner'
 
-import { ThemeProvider } from './components/theme/theme-provider'
+import { ThemeProvider } from '@/components/theme/theme-provider'
+import { Toaster } from '@/components/ui/sonner'
+
 import { router } from './routes'
 
 export function App() {
---
+++ src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,43 @@
+import { ArrowRight, Search, X } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { TableCell, TableRow } from '@/components/ui/table'
+
+// export interface OrderTableRowProps {}
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <Search className="h-3 w-3" />
+          <span className="sr-only">Order details</span>
+        </Button>
+      </TableCell>
+      <TableCell className="font-mono text-xs font-medium">
+        9876eqf87ewfqt68qwd
+      </TableCell>
+      <TableCell className="text-muted-foreground">15 minutes ago</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400" />
+          <span>Pending</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">Allan Goncalves</TableCell>
+      <TableCell className="font-medium">$35.00</TableCell>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-2 h-3 w-3" />
+          Approve
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="mr-2 h-3 w-3" />
+          Cancel
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
