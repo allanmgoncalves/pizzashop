eace1dbd645c412fa871e5d1bcc787f7ad2335ee
Allan Gonçalves <allangoncalves.adm@gmail.com>
2024-07-30 10:45:27 -0300

    add register restaurant

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './global.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
 import { ThemeProvider } from '@/components/theme/theme-provider'
 import { Toaster } from '@/components/ui/sonner'
 
+import { queryClient } from './lib/react-query'
 import { router } from './routes'
 
 export function App() {
@@ -14,7 +16,9 @@ export function App() {
       <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
         <Toaster richColors />
         <Helmet titleTemplate="%s | pizza.shop" />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
